9cbfca745b517a46d1bd21b69c3781a90b0d4f49
Om Prakash Dhiraj <om.dhiraj@ad.infosys.com>
2023-09-06 11:52:56 +0530

    all single rtl query

--- src/components/application/Application.js
+++ src/components/application/Application.js
@@ -1,31 +1,43 @@
 import React from "react";
-
+import Tree from "./../../assets/tree.jpg";
 const Application = () => {
   return (
-    <form>
-      <div>
-        <label htmlFor="name">Name: </label>
-        <input type="text" id="name" />
-      </div>
-      <div>
-        <label htmlFor="job-location"> Job Location: </label>
-        <select id="job-location">
-          <option value="">Select a country</option>
-          <option value="IN">INDIA</option>
-          <option value="US">US a country</option>
-          <option value="CA">canada</option>
-          <option value="AU">Australia</option>
-          <option value="EU">Europe</option>
-          <option value="AF">Africa</option>
-        </select>
-      </div>
-      <div>
-        <label>
-          <input type="checkbox" id="terms" /> I agree TNC
-        </label>
-      </div>
-      <button>Submit</button>
-    </form>
+    <>
+      <h1>Job Application form</h1>
+      <h2>Section 1</h2>
+      <p>All feilds are mandatory</p>
+      <span title="close">X</span>
+      <img src={Tree} alt="Tree with sun" />
+      <div data-testid="custom-element">Custom Html Element</div>
+      <form>
+        <div>
+          <label htmlFor="name">Name: </label>
+          <input type="text" id="name" placeholder="Fullname" value={"om"} onChange={() => {}} />
+        </div>
+        <div>
+          <label htmlFor="bio">Bio</label>
+          <textarea name="bio" id="bio"></textarea>
+        </div>
+        <div>
+          <label htmlFor="job-location"> Job Location: </label>
+          <select id="job-location">
+            <option value="">Select a country</option>
+            <option value="IN">INDIA</option>
+            <option value="US">US a country</option>
+            <option value="CA">canada</option>
+            <option value="AU">Australia</option>
+            <option value="EU">Europe</option>
+            <option value="AF">Africa</option>
+          </select>
+        </div>
+        <div>
+          <label>
+            <input type="checkbox" id="terms" /> I agree TNC
+          </label>
+        </div>
+        <button>Submit</button>
+      </form>
+    </>
   );
 };
 
--- src/components/application/application.test.js
+++ src/components/application/application.test.js
@@ -4,14 +4,62 @@ import Application from "./Application";
 describe("Application", () => {
   test("render correctlly", () => {
     render(<Application />);
-    const nameElement = screen.getByRole("textbox");
+
+    const pageHeading = screen.getByRole("heading", {
+      name: "Job Application form",
+    });
+    expect(pageHeading).toBeInTheDocument();
+
+    const sectionHeading = screen.getByRole("heading", {
+      level: 2,
+    });
+    expect(sectionHeading).toBeInTheDocument();
+
+    //by getbytext
+    const paragraphElement = screen.getByText("All feilds are mandatory");
+    expect(paragraphElement).toBeInTheDocument();
+
+    //getbytitle
+    const closeElement = screen.getByTitle("close");
+    expect(closeElement).toBeInTheDocument();
+
+    //getbyalttext
+
+    const imgElement = screen.getByAltText("Tree with sun");
+    expect(imgElement).toBeInTheDocument();
+
+    //getbytestid
+    const customElement = screen.getByTestId("custom-element");
+    expect(customElement).toBeInTheDocument();
+
+    // const nameElement = screen.getByRole("textbox");
+    const nameElement = screen.getByRole("textbox", { name: "Name:" });
     expect(nameElement).toBeInTheDocument();
+
+    //getbylabeltext
+    const nameElement2 = screen.getByLabelText("Name:");
+    expect(nameElement2).toBeInTheDocument();
+    // getbyplaceholder
+    const nameElement3 = screen.getByPlaceholderText("Fullname");
+    expect(nameElement3).toBeInTheDocument();
+
+    //getbydisplayvalue
+    const nameElement4 = screen.getByDisplayValue("om");
+    expect(nameElement4).toBeInTheDocument();
+
+    const bioElement = screen.getByRole("textbox", { name: "Bio" });
+    expect(bioElement).toBeInTheDocument();
 
     const jobLocationElement = screen.getByRole("combobox");
     expect(jobLocationElement).toBeInTheDocument();
 
     const termElement = screen.getByRole("checkbox");
     expect(termElement).toBeInTheDocument();
+
+    //getbylabeltext
+
+    const termElement2 = screen.getByLabelText("I agree TNC");
+    expect(termElement2).toBeInTheDocument();
 
     const submitButton = screen.getByRole("button");
     expect(submitButton).toBeInTheDocument();
