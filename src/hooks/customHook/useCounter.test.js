bfe9f543a9fea369460979ee8edc6c8a265543cf
Om Prakash Dhiraj <om.dhiraj@ad.infosys.com>
2023-09-07 14:16:38 +0530

    custom hook test

---
+++ src/hooks/customHook/useCounter.test.js
@@ -0,0 +1,25 @@
+import { act, renderHook } from "@testing-library/react";
+import { useCounter } from "./useCounter";
+
+describe("usecounter", () => {
+  test("should render initial count", () => {
+    const { result } = renderHook(useCounter);
+    expect(result.current.count).toBe(0);
+  });
+
+  test("should accept and render the same initial count", () => {
+    const { result } = renderHook(useCounter, { initialProps: 10 });
+    expect(result.current.count).toBe(10);
+  });
+
+  test("should increment the count", () => {
+    const { result } = renderHook(useCounter);
+    act(() => result.current.increment());
+    expect(result.current.count).toBe(1);
+  });
+  test("should decrement the count by 1", () => {
+    const { result } = renderHook(useCounter);
+    act(() => result.current.decrement());
+    expect(result.current.count).toBe(-1);
+  });
+});
